33d88e12dc85b4bb8b71c246d88a6ddfecb995c0
MaskoMasko <persic.massimo@gmail.com>
2022-07-11 21:39:57 +0200

    yes

--- src copy/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { View } from "./View";
-export { Text } from "./Text";
--- src copy/utils/colorsToTxt.ts
+++
@@ -1,5 +0,0 @@
-export function colorsToTxt(colors: any): string[] {
-  let colorsTxt: string[] = [];
-  Object.keys(colors).map((color) => colorsTxt.push("" + color));
-  return colorsTxt;
-}
